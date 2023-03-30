a980e7c460f3fb6fbeabaf0783e271310ee76cd4
Soriful Alam <58595744+sorifulalam@users.noreply.github.com>
2023-03-30 20:15:43 +0600

    cart calculation done

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -2,14 +2,22 @@ import React from 'react';
 import './Cart.css'
 const Cart = (props) => {
     const cart = props.cart;
+    let totalPrice = 0 ;
+    let totalShipping = 0;
+    for(const product of cart){
+        totalPrice =totalPrice+product.price;
+        totalShipping=  totalShipping + product.shipping;
+    }
+    const tax = totalPrice*7/100;
+    const grandtotal = totalPrice+totalShipping+tax
     return (
         <div className='cart'>
             <h4>Order-Summary</h4>
             <p>Selected Items: {cart.length}</p>
-            <p>Total Price: </p>
-            <p>Total Shipping: </p>
-            <p>Tax: </p>
-            <h6>Grand Total: </h6>
+            <p>Total Price: ${totalPrice}</p>
+            <p>Total Shipping: ${totalShipping}</p>
+            <p>Tax: ${tax}</p>
+            <h6>Grand Total: {grandtotal}</h6>
             
         </div>
     );
